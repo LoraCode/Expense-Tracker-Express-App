2f4637131d0b574ab43cf214c2c922ab27dede99
Lora <jonathan.lora.95@gmail.com>
2018-08-02 23:17:32 -0400

    db configuration and created app directory

---
+++ expense-tracker-app/config/dbConfig.js
@@ -0,0 +1,12 @@
+// DATABASE CONFIGURATION
+
+const config = {
+  // URL where database is located
+  host: process.env.DB_HOST || 'localhost',
+  // Port where database is configured
+  port: process.env.DB_PORT || 5432,
+  // Database name
+  database: process.env.DB_NAME || 'expenses_dev',
+};
+
+module.exports = config;
---
+++ expense-tracker-app/config/dbConnection.js
@@ -0,0 +1,8 @@
+// PG-PROMISE CONFIGURATION
+
+// Importing database
+const config = require('./dbConfig');
+// Init pg-promise
+const pgp = require('pg-promise')();
+// Connecting databse with pg-promise
+module.exports = pgp(config);
